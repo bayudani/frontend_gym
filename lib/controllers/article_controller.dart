4416ad79a7287731760cb93766b1ab6420bce2d6
bayudani <bayu22122017@gmail.com>
2025-06-07 13:27:40 +0700

    article init

---
+++ lib/controllers/article_controller.dart
@@ -0,0 +1,4 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import '../models/user_models.dart';
+import '../service/api_service.dart';
---
+++ lib/views/article/article.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import 'package:gym_app/views/auth/register_page.dart'; 
+import '../../controllers/auth_controller.dart';   
